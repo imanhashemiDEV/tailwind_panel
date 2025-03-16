88e40f06a0b427c018100ccbe9a56b0634d7d8c5
iman hashemi <hashemi.iman@gmail.com>
2025-03-16 17:01:01 +0330

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -84,9 +84,7 @@ module.exports = {
         },
     },
     plugins: [
-        require('@tailwindcss/forms')({
-            strategy: 'class',
-        }),
+       
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
     ],
